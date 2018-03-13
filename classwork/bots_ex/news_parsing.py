3bad69d3e1912d082b0f9378e7492a4a33eea9d1
haskone <3598179+haskone@users.noreply.github.com>
2018-03-14 00:49:57 +0200

    Last modifications

--- classwork/bots_ex/news_parsing.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-ycomb = "https://news.ycombinator.com/"
-soup = BeautifulSoup(requests.get(ycomb).content)
-
-print(soup.findAll("a", {"class": "storylink"})[0])
