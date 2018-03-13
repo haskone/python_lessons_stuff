b73e511620055c856c1867159ee73b3149dd645c
haskone <3598179+haskone@users.noreply.github.com>
2018-03-13 19:48:50 +0200

    Weather bot

--- classwork/bots_ex/telepot_simple.py
+++ classwork/bots_ex/telepot_simple.py
@@ -1,32 +1,76 @@
 import time
 import random
 import datetime
+import requests
+
+from weather import Weather
+from bs4 import BeautifulSoup
+
 import telepot
 from telepot.loop import MessageLoop
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
+
+
+def get_weather(location):
+    weather = Weather(unit='c')
+    location = weather.lookup_by_location(location=
+        f'({location["latitude"]},{location["longitude"]})')
+
+    forecasts = location.forecast()
+    city = location.location().city()
+    results = []
+    for f in forecasts:
+        results.append(f'{city}: {f.date()} | {f.text()} | '
+                       f'{f.high()} | {f.low()}')
+    return results
 
 
 def get_news():
-    pass
+    ycomb = "https://news.ycombinator.com/"
+    soup = BeautifulSoup(requests.get(ycomb).content)
+    return [f"{i.get_text()} ({i.get('href')})"
+            for i in soup.findAll(
+            "a", {"class": "storylink"})]
 
 
 def handle(msg):
     chat_id = msg['chat']['id']
-    command = msg['text']
+    location = msg.get('location', None)
 
-    print('Got command: %s' % command)
+    if location is not None:
+        print(f"Got location: {location}")
+        for day in get_weather(location=location):
+            bot.sendMessage(chat_id, day)
 
-    if command == '/roll':
-        bot.sendMessage(chat_id, random.randint(1,6))
-    elif command == '/time':
-        bot.sendMessage(chat_id, str(datetime.datetime.now()))
-    elif command == '/news':
-        for piece in get_news()[:-3]:
-            bot.sendMessage(chat_id, piece)
-    else:
-        bot.sendMessage(chat_id, "got it, but can't understand")
+    text = msg.get('text', None)
+
+    if text is not None:
+        print(f'Got text: {text}')
+        if text == '/roll':
+            bot.sendMessage(chat_id, random.randint(1,6))
+        elif text == '/time':
+            bot.sendMessage(chat_id, str(datetime.datetime.now()))
+        elif text == '/news':
+            for piece in get_news()[:3]:
+                bot.sendMessage(chat_id, piece)
+        elif text == '/key':
+            bot.sendMessage(chat_id, 'sendind location by request...',
+                            reply_markup=ReplyKeyboardMarkup(
+                                keyboard=[
+                                    [KeyboardButton(
+                                        text='Location by request',
+                                        request_location=True)]
+                                ]
+                        ))
+        elif text == 'Location by request':
+            bot.sendMessage(chat_id, 'Ok')
+        else:
+            bot.sendMessage(chat_id,
+                            "got text command, "
+                            "but can't understand")
 
 
-bot = telepot.Bot('your_token')
+bot = telepot.Bot('TOKEN')
 
 MessageLoop(bot, handle).run_as_thread()
 print('I am listening ...')
